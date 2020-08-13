37ba85f273f87362d7927feddd4de0235cde36bd
Jose A Alvarado-Guzman <jaa2220@columbia.edu>
2020-08-13 10:31:45 -0400

    Install the plyr R library

--- install_r_notebook_pakages.R
+++ install_r_notebook_pakages.R
@@ -1,3 +1,3 @@
-install.packages(c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest','tidyverse'))
+install.packages(c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest','tidyverse','plyr'))
 devtools::install_github('IRkernel/IRkernel')
 IRkernel::installspec(user = FALSE)
